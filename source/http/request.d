460568c345e869e8f2704bb304f62b1c60b7acf2
David <dkdewitt@gmail.com>
2015-08-25 20:22:26 -0700

    Cleaned up request parsing

--- source/http/request.d
+++ source/http/request.d
@@ -6,6 +6,7 @@ import std.algorithm;
 import std.range;
 import std.string;
 import std.typecons;
+import std.conv;
 class BaseHTTPRequest{
 
     this(){
@@ -25,7 +26,7 @@ class HTTPRequest: BaseHTTPRequest{
     string password;
     string queryString;
     string[string] params;
-    string[string] headers;
+    string[string] _headers;
     URL url;
     string[string] form;  //POST request data
 
@@ -42,44 +43,52 @@ this(char[] data){
        
     }
 
-    void formData(string[] rng){
+    /*void formData(string[] rng){
         auto tmp = splitter(rng[0], "&");
         foreach(item; tmp){
             auto spllitLoc = item.indexOf("=");
             form[item[0..spllitLoc]] = item[spllitLoc+1..$];
         }
-    }
+    }*/
 
     void parseRequest(){
         import std.stdio;
         import std.conv;
-        auto requestTmp = splitter(this.data, "\r\n")
-            .array();
-        
-        foreach(int i, line; requestTmp[1..$]){
-            string headerString = line.dup;
-            if(headerString == ""){
-                string[] requestBody = to!(string[])(requestTmp[i+2..$]);
-                formData(requestBody);
-                break;
-            }
-            auto spllitLoc = headerString.indexOf(':');
-            if (spllitLoc > 0){
-            auto t = tuple(headerString[0..spllitLoc], headerString[spllitLoc+2..$]);
-            headers[t[0]] = t[1];
+        string tmp = this.data.dup;
+        auto rawRequestData = splitLines(tmp).split("");
+
+        string rawRequestLine = rawRequestData[0][0];
+        string[] rawRequestHeaders = rawRequestData[0][1..$];
+        foreach(header; rawRequestHeaders){
+            auto lineSep = header.indexOf(":");
+            if(lineSep < 0)
+                throw new Exception("Invalid Header");
+            
+            this._headers[header[0..lineSep]] = stripLeft(header[lineSep+1..$]) ;
+        }
+        if(rawRequestData[$-1].length == 0)
+            rawRequestData = rawRequestData[0..$-1];
+        if(rawRequestData.length > 1){
+            size_t contentLength = to!size_t(_headers.get("Content-Length", "0"));
+            char[] rawRequestBody = rawRequestData[1][0].dup;
+            string requestBody = rawRequestBody[0..contentLength].dup;
+            auto requestBodyItems = requestBody.split("&");
+
+            foreach(item; requestBodyItems){
+                auto lineSep = item.indexOf("=");
+                if(lineSep < 0)
+                    throw new Exception("Invalid Request Body");
+                this.form[item[0..lineSep]] = item[lineSep+1..$];
             }
         }
-
-        auto requestLine = requestTmp[0].split(" ");
-        string requestType = requestLine[0].dup;
-        this.method = getMethodFromString(requestType);
-        string path = requestLine[1].dup;
-        string urlString = "http://" ~ this.headers.get("Host", "") ~ path;
-        this.fullPath = urlString;
-        this.path = path;
-      
+            auto requestLine = rawRequestLine.split(" ");
+            string requestType = requestLine[0].dup;
+            this.method = getMethodFromString(requestType);
+            string path = requestLine[1].dup;
+            string urlString = "http://" ~ this._headers.get("Host", "") ~ path;
+            this.fullPath = urlString;
+            this.path = path;
     }
-
 }
 
 
